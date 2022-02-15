6f4fbc67b9b441bbde6c13b0326aed45ec3f4b42
ArthurYegoryan <arthur.yegoryan.97@gmail.com>
2022-02-15 17:14:23 +0400

    Lesson_1 1-st commit

---
+++ Lessons_Homeworks/1_Lesson.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myWorks
+{
+    internal class Lesson_1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World");
+
+            Console.Write("What is your name?  ");
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Hello {name}!");
+
+            Console.ReadKey();
+        }
+    }
+}
