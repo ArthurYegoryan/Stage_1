50eef61bdc562dc494574efdcba2676e99f1613d
ArthurYegoryan <arthur.yegoryan.97@gmail.com>
2022-03-03 17:51:52 +0400

    Added Constructor parts in 8_Lesson_Encapsulation_Constructor.cs file

---
+++ Lessons_Homeworks/Lesson_8_Classes_Structs/8_Lesson_Encapsulation_Constructor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons_Homeworks.Lesson_8_Classes_Structs
+{
+    class Gun                                                  
+    {                                                          
+        public Gun(bool isLoaded)                             //   ctor ու 2 tab-ով ստեղծում ենք constructor
+        {                                                     //  constructor-ի մեջից կարեղ ենք private արժեքները փոխել
+            _isLoaded = isLoaded;                             //  և ստեղծված instance-ները արդեն կընդունեն constructor-ի փոխած արժեքները  
+        }                                                     //     Constructor-ի շնորհիվ չենք կորցնում ինկապսուլյացիայի իմաստը
+        
+        private bool _isLoaded;                                 
+
+        private void Reload()
+        {
+            Console.WriteLine("Reloading...");                  //     Ինկապսուլյացիան այն է, որ class-ից պետք է դոստուպ տալ միայն այն օբյեկտներին,
+                                                                //  որոնցից օգտվում ենք (սխալներից խուսափելու համար)
+            _isLoaded = true;                                   //  Կազմակերպում ենք access modifier-ների օգնությամբ
+
+            Console.WriteLine("Reloaded!");
+        }
+
+        public void Shot()
+        {
+            if (_isLoaded == false)
+            {
+                Console.WriteLine("Gun isn't reloaded!");
+                Reload();
+            }
+            Console.WriteLine("Ddsh-ddsh!!!");
+            _isLoaded = false;
+        }
+    }
+    internal class _8_Lesson_Encapsulation
+    {
+        static void Main()
+        {
+            Gun gun = new Gun(isLoaded: true);  //  կամ  Gun(true)       //  Կարող ենք պարամետրով տալ constructor-ի արժեքը, եթե տալիս ենք true, ուրեմն լիցքավորված է և
+            gun.Shot();                                                  // միանգամից կրակում է, եթե տալիս ենք false, ուրեմն լիցքավորում է նոր է կրակում
+        }
+    }
+}
