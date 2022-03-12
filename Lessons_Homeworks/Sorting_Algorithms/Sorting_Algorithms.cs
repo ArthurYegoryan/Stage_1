01984f68d4bbdfa275ae7844682728a75eb8ed7b
ArthurYegoryan <arthur.yegoryan.97@gmail.com>
2022-03-12 22:45:56 +0400

    Added Sorting_Algorithms.cs file in Sorting_Algorithms folder

---
+++ Lessons_Homeworks/Sorting_Algorithms/Sorting_Algorithms.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons_Homeworks
+{
+    internal class SortingAlgorithms
+    {
+        public static void Bubble_Sort(int[] nums)
+        {            
+            for (int i = 0; i < nums.Length; i++)
+            {
+                bool isSorted = true;
+                for (int j = 0; j < nums.Length - 1; j++)
+                {
+                    if (nums[j] > nums[j + 1])
+                    {
+                        isSorted = false;
+                        int temp = nums[j];
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = temp;
+                    }
+                }
+                if (isSorted)
+                    break;
+            }
+        }
+
+        public static void Selection_Sort(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {                
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[j] < nums[i])
+                    {
+                        int temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }                        
+                }
+            }
+        }
+
+        public static void Insertion_Sort(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                while (nums[i] < nums[i - 1])
+                {
+                    int temp = nums[i];
+                    nums[i] = nums[i - 1];
+                    nums[i - 1] = temp;
+                    i--;
+                    if (i == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+        static void Main()
+        {
+            Console.Write("Enter array size: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            int[] nums = new int[size];
+
+            Console.WriteLine("Enter array numbers:");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            //Bubble_Sort(nums);
+            //Selection_Sort(nums);
+            Insertion_Sort(nums);
+
+            Console.WriteLine("Sorted array is:");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write(nums[i] + " ");
+            }
+        }
+    }
+}
--- Lessons_Homeworks/Tasks/Stage_1_Exam_Task_3.cs
+++ Lessons_Homeworks/Tasks/Stage_1_Exam_Task_3.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lessons_Homeworks.Tasks
+/*namespace Lessons_Homeworks.Tasks
 {
     struct ConvertToString
     {
@@ -30,7 +30,7 @@ namespace Lessons_Homeworks.Tasks
         {
             Console.Write("Enter array size: ");
             int size = Convert.ToInt32(Console.ReadLine());
-            
+
             int[] numbers = new int[size];
 
             Console.WriteLine("Enter numbers array elements:");
@@ -47,11 +47,11 @@ namespace Lessons_Homeworks.Tasks
             {
                 Console.Write(numbers[i] + " ");
             }
-            Console.WriteLine();            
+            Console.WriteLine();
 
             ConvertToString convertToString = new ConvertToString(size, numbers);
-                        
-            convertToString.DoConvert(size, numbers); 
+
+            convertToString.DoConvert(size, numbers);
 
             Console.WriteLine($"NumbersStr array elements type is: {convertToString.numbersStr.GetType()}");
             Console.Write("Elements of numbers array are: ");
@@ -61,6 +61,6 @@ namespace Lessons_Homeworks.Tasks
                 Console.Write(convertToString.numbersStr[i] + " ");
             }
             Console.WriteLine();
-        }            
+        }
     }
-}
+}*/
